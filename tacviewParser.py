6abc04f370f9029d75d4fd9336168a0d7bc95d1c
dpassoni <davide.passoni@positech.it>
2021-11-18 08:04:00 +0100

    Added events

--- tacviewParser.py
+++ tacviewParser.py
@@ -1,10 +1,10 @@
+from types import LambdaType
 import zipfile
 import properties as properties
 import datetime
 import logging
-
-def nearest(items, pivot):
-    return min(items, key=lambda x: abs(x - pivot))
+import geopy.distance
+import numpy as np
 
 class TimeProperty(dict):
     @property
@@ -14,6 +14,33 @@ class TimeProperty(dict):
     @property
     def vals(self):
         return list(self.values())
+
+    def nearest(self, time):
+        nearestTime = min(self.times, key=lambda x: abs(x - time))
+        return self[nearestTime]
+
+class Position(TimeProperty):
+    def decode(self, line, time, reference):
+        tr = line.split("|")
+        props = properties.TRANSFORMATION_PROPERTIES[len(tr)]
+        referencePosition = {'Longitude': reference.getProperty("ReferenceLongitude"), 'Latitude': reference.getProperty("ReferenceLatitude"), 'Altitude': 0, 'Roll': 0, 'Pitch': 0, 'Yaw': 0, 'U': 0, 'V': 0, 'Heading': 0}
+        
+        if time not in self:
+            if len(self.times) == 0:
+                self[time] = {'Longitude': 0, 'Latitude': 0, 'Altitude': 0, 'Roll': 0, 'Pitch': 0, 'Yaw': 0, 'U': 0, 'V': 0, 'Heading': 0}
+            else:
+                self[time] = self[self.times[-1]].copy()
+
+        for i in range(len(tr)):
+            prop = props[i]
+            if tr[i] != '':
+                self[time][prop] = float(tr[i]) + referencePosition[prop]
+
+    def distance(self, pivot):
+        distances = TimeProperty()
+        for time in self.times:
+            distances[time] = geopy.distance.great_circle((self[time]["Latitude"], self[time]["Longitude"]), pivot).nm
+        return distances
 
 class FileHandler():
     def __init__(self, path) -> None:
@@ -45,7 +72,9 @@ class Object():
     def __init__(self, id, reference) -> None:
         self._id = id
         self._reference = reference
-        self._events = {}
+        self._events = TimeProperty()
+        self._parent = None
+        self.Position = Position()
         if self._id == 0:
             for prop in properties.GLOBAL_PROPERTIES:
                 setattr(self, prop, TimeProperty())
@@ -56,18 +85,20 @@ class Object():
     @property
     def reference(self):
         return self._reference
+    
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, newParent):
+        self._parent = newParent
 
     def decode(self, arr):
         for e in arr:
             val = e.split("=")
             if val[0] == "T":
-                tr = val[1].split("|")
-                props = properties.TRANSFORMATION_PROPERTIES[len(tr)]
-                for i in range(len(tr)):
-                    prop = props[i]
-                    if hasattr(self, prop):
-                        if tr[i] != '':
-                            getattr(self, prop)[self._reference.timeNow] = float(tr[i])
+                self.Position.decode(val[1], self._reference.timeNow, self._reference)
             else:
                 if hasattr(self, val[0]):
                     if val[1] != '':
@@ -75,21 +106,22 @@ class Object():
 
     def getProperty(self, prop, propertyTime = None):
         if hasattr(self, prop):
-            if len(list(getattr(self, prop).values())) == 0: return None
-            elif len(list(getattr(self, prop).values())) == 1:
-                return list(getattr(self, prop).values())[0]
+            attr = getattr(self, prop)
+            if len(attr.vals) == 0: return None
+            elif len(attr.vals) == 1:
+                return attr[attr.times[0]]
             else:
                 v = getattr(self, prop)
                 if propertyTime is None:
-                    return getattr(self, prop)
+                    return attr
                 else:
-                    return v[nearest(list(v.keys()), propertyTime)]
+                    return v.nearest(propertyTime)
 
     def addEvent(self, time, event: Event):
         self._events[time] = event
 
     def getEventsByType(self, eventType: str):
-        events = {}
+        events = TimeProperty()
         for time, event in self._events.items():
             if event.eventType == eventType:
                 events[time] = event
@@ -97,7 +129,7 @@ class Object():
 
 class Reference(Object):
     _timeNow = datetime.timedelta(0, 0)  
-    _ReferenceTime = {}
+    _ReferenceTime = TimeProperty()
     
     def __init__(self, id) -> None:
         super().__init__(id, self)
@@ -156,10 +188,25 @@ class TacviewParser():
                         obj.decode(arr[1:])
                         obj.addEvent(self._reference.timeNow, Event("Created", ""))
                         self._objects[id] = obj
+                        if "Weapon" in obj.getProperty('Type'):
+                            obj.parent = self.findParent(obj)
                         logging.debug(f"TacviewParser: Created object with ID = {id} at time = {self._reference.timeNow}, pilot = {obj.getProperty('Pilot')}, type = {obj.getProperty('Type')}")
                 else:
                     obj = self._objects[id]
                     obj.decode(arr[1:])
+
+    def findParent(self, obj: Object):
+        distances = {}
+        time = obj.getEventsByType("Created").times[0]
+        objPos = (obj.getProperty("Position", time)["Latitude"], obj.getProperty("Position", time)["Longitude"])
+        for id, par in self.objects.items():
+            if id != 0:
+                if "Air" in par.getProperty('Type') or "Ground" in par.getProperty('Type') or "Sea" in par.getProperty('Type'):
+                    parPos = (par.getProperty("Position", time)["Latitude"], par.getProperty("Position", time)["Longitude"])
+                    distances[par] = geopy.distance.great_circle(objPos, parPos).nm
+
+        return list(distances.keys())[np.argmin(list(distances.values()))]
+
 
     @property
     def objects(self):
